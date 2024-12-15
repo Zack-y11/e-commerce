175bcfc1af694e7718dda6c5aabc810fa54ada4e
Zack-y11 <dev.zacknmr28@gmail.com>
2024-12-14 18:47:08 -0600

    add user routes and cookie parser middleware

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,18 @@
 import {connect} from './db/posgres';
 import express, { query } from 'express';
+import user from './routes/User.Routes';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
 connect(); // This will connect to the database
 app.use(express.json());
-app.get('/', (req, res) => {
+app.use(cookieParser());
+
+app.get('/api', (req, res) => {
     res.status(200).json({message: 'Hello World'});
 });
+app.use('/api', user);
 
 app.listen(3000, () => {
     console.log('Server running on port 3000');

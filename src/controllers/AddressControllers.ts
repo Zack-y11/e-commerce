f199a6424663ec5800f3abf306e284529d51e68f
Zack-y11 <dev.zacknmr28@gmail.com>
2024-12-17 14:55:31 -0600

    feat: implement CRUD operations for shipping addresses; add IShippingAddress interface

---
+++ src/controllers/AddressControllers.ts
@@ -0,0 +1,156 @@
+import IShippingAddress from "../types/IAddress";
+import { Request, Response } from "express";
+import { client } from "../db/posgres";
+
+//shipping_addresses CRUD
+export const createShippingAddress = async (req: Request, res: Response) => {
+  try {
+    const userCookiesId = req.cookies.id;
+
+    if (!userCookiesId) {
+      res.status(401).json({ message: "Unauthorized" });
+      return;
+    }
+    const user_id = JSON.parse(userCookiesId).id;
+    let is_default = true;
+    let userHasAddress = await client.query(
+      "SELECT * FROM shipping_addresses WHERE user_id = $1",
+      [user_id]
+    );
+    let {
+      address_line1,
+      address_line2,
+      city,
+      state,
+      postal_code,
+      country,
+    }: IShippingAddress = req.body;
+    if (userHasAddress.rows.length > 0) {
+      res.status(400).json({ message: "User already has a shipping address" });
+      is_default = false;
+    }
+    const response = await client.query(
+      "INSERT INTO shipping_addresses (user_id, address_line1, address_line2, city, state, postal_code, country, is_default) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *",
+      [
+        user_id,
+        address_line1,
+        address_line2,
+        city,
+        state,
+        postal_code,
+        country,
+        is_default,
+      ]
+    );
+    res.status(201).json({ address: response.rows[0] });
+  } catch (error) {
+    res.status(500).json({
+      error: error,
+      message: "Internal Server Error",
+    });
+    return;
+  }
+};
+
+export const getShippingAddresses = async (req: Request, res: Response) => {
+  try {
+    const userCookiesId = req.cookies.id;
+    const address_id = req.params.id;
+
+    if (!userCookiesId) {
+      res.status(401).json({ message: "Unauthorized" });
+      return;
+    }
+    const user_id = JSON.parse(userCookiesId).id;
+
+    const response = await client.query(
+      "SELECT * FROM shipping_addresses WHERE user_id = $1 AND id = $2",
+      [user_id, address_id]
+    );
+    res.status(200).json(response.rows);
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+};
+
+export const updateShippingAddress = async (req: Request, res: Response) => {
+  try {
+    const userCookiesId = req.cookies.id;
+
+    if (!userCookiesId) {
+      res.status(401).json({ message: "Unauthorized" });
+      return;
+    }
+    const user_id = JSON.parse(userCookiesId).id;
+    const {
+      address_line1,
+      address_line2,
+      city,
+      state,
+      postal_code,
+      country,
+    }: IShippingAddress = req.body;
+    const address_id = req.params.id;
+
+    const userHasAddress = await client.query(
+      "SELECT * FROM shipping_addresses WHERE id = $1 AND user_id = $2",
+      [address_id, user_id]
+    );
+
+    if (userHasAddress.rows.length === 0) {
+      res.status(404).json({ message: "Address not found" });
+      return;
+    }
+
+    const response = await client.query(
+      "UPDATE shipping_addresses SET address_line1 = $1, address_line2 = $2, city = $3, state = $4, postal_code = $5, country = $6 WHERE id = $7 AND user_id = $8 RETURNING *",
+      [
+        address_line1,
+        address_line2,
+        city,
+        state,
+        postal_code,
+        country,
+        address_id,
+        user_id,
+      ]
+    );
+
+    res.status(200).json({
+      message: "Address updated successfully",
+      address: response.rows[0],
+    });
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+};
+
+export const deleteShippingAddress = async (req: Request, res: Response) => {
+  try {
+    const address_id = req.params.id;
+
+    if (!address_id) {
+      res.status(400).json({ message: "Address id is required" });
+      return;
+    }
+    const data = await client.query(
+      "SELECT * FROM shipping_addresses WHERE id = $1",
+      [address_id]
+    );
+    if (data.rows.length === 0) {
+      res.status(404).json({ message: "Address not found" });
+      return;
+    }
+    const response = await client.query(
+      "DELETE FROM shipping_addresses WHERE id = $1",
+      [address_id]
+    );
+    res.status(200).json({ message: "Address deleted successfully" });
+  } catch (error) {
+    res.status(500).json({
+      error: error,
+      message: "Internal Server Error",
+    });
+    return;
+  }
+};
---
+++ src/types/IAddress.ts
@@ -0,0 +1,10 @@
+export default interface IShippingAddress {
+    user_id: number;
+    address_line1: string;
+    address_line2?: string;
+    city: string;
+    state: string;
+    postal_code: string;
+    country: string;
+    is_default: boolean;
+}

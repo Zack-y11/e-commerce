5475c202a9544f7f5e7b73f12a737ebe8454a380
Zack-y11 <dev.zacknmr28@gmail.com>
2025-01-30 13:01:08 -0600

    feat: refactor Order and PaymentsControllers to use Supabase for order and payment operations

--- src/routes/Order.Routes.ts
+++ src/routes/Order.Routes.ts
@@ -13,13 +13,13 @@ import auth from "../middleware/auth";
 
 const router = Router();
 
-router.post("/order", auth, createOrder);
-router.put("/order/:id", auth, updateOrder);
-router.get("/order/:id", auth, getOrderById);
-router.get("/order", auth, getOrders);
-router.delete("/order/:id", auth, deleteOrder);
-router.post("/order/:idOrder", auth, addItemsToOrder);
-router.delete("/order/remove/:idOrder", auth, removeItemsFromOrder);
+router.post("/orders", auth, createOrder);
+router.put("/orders/:id", auth, updateOrder);
+router.get("/orders/:id", auth, getOrderById);
+router.get("/orders", auth, getOrders);
+router.delete("/orders/:id", auth, deleteOrder);
+router.post("/orders/:idOrder", auth, addItemsToOrder);
+router.delete("/ordes/remove/:idOrder", auth, removeItemsFromOrder);
 
 //test
 router.get("/order/info/:id", auth, getOrderWithInfo);
